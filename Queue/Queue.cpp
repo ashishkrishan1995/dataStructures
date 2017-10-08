b7ff815f1d24ed941e5e3216b317f04abbd00df1
vaibhaw2731 <vaibhaw2731@yahoo.com>
2017-10-09 03:35:55 +0530

    Update Queue.cpp

--- Queue/Queue.cpp
+++ Queue/Queue.cpp
@@ -2,19 +2,19 @@
 
 using namespace std;
 
-struct QueueNode             //Node of queue
+struct QueueNode      	//Node of queue
 {
 	int data;
 	struct QueueNode *next;
-}*front,*rear;				//front and rear node reference of the queue
+}*front,*rear;		//front and rear node reference of the queue
 
-void initialize()			//intializing front and rear
+void initialize()	//intializing front and rear
 {
 	front=NULL;
 	rear=NULL;
 }
 
-void menu()					//menu to select an action to perform
+void menu()	//menu to select an action to perform
 {
 	cout<<"Enter 1 to insert an element to the queue"<<endl;
 	cout<<"Enter 2 to remove an item from the queue"<<endl;
@@ -23,9 +23,9 @@ void menu()					//menu to select an action to perform
 	cout<<"Enter your choice: ";
 }
 
-void insert(int key)					//insert an element in the queue
+void insert(int key)		//insert an element in the queue
 {
-	QueueNode *temp=new QueueNode();    //temporary node to store the given element
+	QueueNode *temp=new QueueNode();    	 //temporary node to store the given element
 	temp->data=key;
 	temp->next=NULL;
 	if(front==NULL)
@@ -40,7 +40,7 @@ void insert(int key)					//insert an element in the queue
 	}
 }
 
-void remove()							//remove the first element from the queue
+void remove()		//remove the first element from the queue
 {
 		QueueNode *temp;
 		if(front==NULL)
@@ -52,11 +52,11 @@ void remove()							//remove the first element from the queue
 			temp=front;
 			cout<<"Element Deleted: "<<temp->data<<endl;
 			front=front->next;
-			free(temp);					//free up memory occupied by the deleted element
+			free(temp);		//free up memory occupied by the deleted element
 		}
 }
 
-void display()						//display all elements of the queue
+void display()		//display all elements of the queue
 {
 	QueueNode *temp;
 	if(front==NULL)
@@ -76,7 +76,7 @@ void display()						//display all elements of the queue
 	}
 }
 
-int main()						//driver function
+int main()	//driver function
 {
 
 	initialize();
@@ -94,17 +94,17 @@ int main()						//driver function
 		switch(ch)
 		{
 			case 1: int key;
-					cout<<"Enter an element to insert: ";
-					cin>>key;
-					insert(key);
-					cout<<endl;
-					break;
+				cout<<"Enter an element to insert: ";
+				cin>>key;
+				insert(key);
+				cout<<endl;
+				break;
 
 			case 2: remove();
-					break;
+				break;
 
 			case 3: display();
-					break;
+				break;
 
 			default: cout<<"Wrong choice, Try again"<<endl;
 
