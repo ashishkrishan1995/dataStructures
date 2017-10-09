a45fdf74af0415d0b58e68f6bad11d965cdc97d0
hg398 <rockingharsh398@gmail.com>
2017-10-09 11:24:53 -0700

    Added comments in (Graph Implementation & Representation using STL) in Graphs

--- Graphs/Graph Implementation & Representation using STL.cpp
+++ Graphs/Graph Implementation & Representation using STL.cpp
@@ -2,28 +2,28 @@
 
 #include<bits/stdc++.h>
 using namespace std;
-vector <int> adj[100000];
+vector <int> adj[100000];                                                           //vector to represent adjacency lsts of graph
 
 int main() {
-	int n,e;
+	int n,e;                                                                        //n is no. of nodes and e is edges
 	cout<<"Enter number of Nodes and Edges in graph:"<<endl;;
 	cin>>n>>e;
 	cout<<"Enter pairs of vertices between which an edge of graph exist:"<<endl;
 	for(int i=0;i<e;i++)
 	{
-		int a,b;
+		int a,b;                                                                    // vertices between which an edge exist
 		cin>>a>>b;
-		a--;
+		a--;                                                                        
 		b--;
-		adj[a].push_back(b);
+		adj[a].push_back(b);                                                        // push vertices in list of each other (undirected graph)
 		adj[b].push_back(a);
 	}
-	for(int i=0;i<n;i++)
+	for(int i=0;i<n;i++)                                                            
 	{
 		cout<<"Adjacency List of "<<i+1<<" contains --> ";
-		for(int j=0;j<adj[i].size();j++)
+		for(int j=0;j<adj[i].size();j++)                                            //traversing adjacency list of each vertex
 		{
-			cout<<adj[i][j]+1<<" ";
+			cout<<adj[i][j]+1<<" ";                                                 //print the vertex to which it is directly connected 
 		}
 		cout<<endl;
 	}
